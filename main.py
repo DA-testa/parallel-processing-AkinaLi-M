e4b872ce0debb971c30ee6a0a3e9ca0733b2ed88
AkinaLi-M <125566464+AkinaLi-M@users.noreply.github.com>
2023-03-19 11:40:56 +0000

    test2

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def parallel_processing(n, m, data):
         min_thread_time = min(data)
         min_thread_index = data.index(min_thread_time)
         output.append((min_thread_index, min_thread_time))
-        data[min_thread_index] += data[min_thread_index]
+    data[min_thread_index] += data[min_thread_index]
 
     return output
 
